07ba85f8714e2af21646f5734603a3841ae7bc8f
Hara Tomoka <pixieyunomi@gmail.com>
2020-08-24 16:15:18 +0900

    decorated

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
   end
 
   def index
-  	@books = Book.all
+  	@books = Book.all.order(id: "ASC")
   	@book = Book.new
   end
 
@@ -14,9 +14,10 @@ class BooksController < ApplicationController
   def create
   	@book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
     redirect_to book_path(@book.id)
     else
-      @books = Book.all
+      @books = Book.all.order(id: "ASC")
       render :index
     end
   end
@@ -26,15 +27,21 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
+    end
   end
 
   private
